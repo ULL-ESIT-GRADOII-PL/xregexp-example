ff16206d977135d0815bd2590f0d587646a8d70d
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-03-14 11:01:09 +0000

    El devanagari o devanāgarī es una escritura abugida utilizada para escribir el idioma nepalí y varios idiomas de India

--- unicode.js
+++ unicode.js
@@ -4,14 +4,37 @@ var XRegExp = require('xregexp');
 let r = XRegExp('\\p{Sc}\\pN+'); // Sc: currency symbol, N: number
 
 console.log(r.test("€32")); // true
+console.log("€६ (Devanagari 6) matches ", r.test("€६")); // ६ (Devanagari 6) true
 
 // Scripts
 console.log(XRegExp('\\p{Cyrillic}').test("Б")); // true
+
+/*
+ In Unicode, a *script* is a collection of letters and other written
+ signs used to represent textual information in one or more writing
+ systems.
+
+ Some *scripts* support one and only one writing system
+ and language, for example, Armenian. 
+ 
+ Other scripts support many different writing systems; for example, the Latin script supports
+ English, French, German, Italian, Vietnamese, Latin itself, and
+ several other languages. Some languages make use of multiple
+ alternate writing systems, thus also use several scripts.
+
+ Unicode can assign a character in the UCS to a single script only.
+ However, many characters — those that are not part of a formal
+ natural language writing system or are unified across many writing
+ systems may be used in more than one script. For example, currency
+ signs, symbols, numerals and punctuation marks. In these cases
+ Unicode defines them as belonging to the "common" script
+ */
 console.log(XRegExp('[\\p{Latin}\\p{Common}]').test("ñ❦")); // Floral heart u2766
 
 // Blocks (use 'In' prefix)
 XRegExp('\\p{InLatinExtended-A}');
 XRegExp('\\P{InPrivateUseArea}'); // Uppercase \P for negation
+
 console.log("Mongolian "+XRegExp('\\p{^InMongolian}').test("ᠠ")); // False: Alternate negation syntax Unicode Character 'MONGOLIAN LETTER A' (U+1820)
 
 // Properties
@@ -28,12 +51,26 @@ console.log(unicodeWord.test("العربية")); // true
 console.log(XRegExp("^\\p{Katakana}+$").test("カタカナ")); // true
 
 /*
-By default, \p{…} and \P{…} support the Basic Multilingual Plane (i.e. code points up to U+FFFF). You can opt-in to full 21-bit Unicode support (with code points up to U+10FFFF) on a per-regex basis by using flag A. In XRegExp, this is called astral mode. You can automatically add flag A for all new regexes by running XRegExp.install('astral'). When in astral mode, \p{…} and \P{…} always match a full code point rather than a code unit, using surrogate pairs for code points above U+FFFF.
+  By default, \p{…} and \P{…} support the Basic Multilingual Plane
+  (i.e. code points up to U+FFFF). 
+  
+  You can opt-in to full 21-bit Unicode support (with code points
+  up to U+10FFFF) on a per-regex basis by using flag A. 
+  
+  In XRegExp, this is called astral mode. 
+  
+  You can automatically add flag A for all new regexes by running
+  XRegExp.install('astral'). 
+  
+  When in astral mode, \p{…} and \P{…} always match a full code point rather than a code unit, using
+  surrogate pairs for code points above U+FFFF.
 */
 
 // Using flag A to match astral code points
-XRegExp('^\\pS$').test('💩'); // -> false
-XRegExp('^\\pS$', 'A').test('💩'); // -> true
+
+console.log("Astral option");
+console.log(XRegExp('^\\pS$').test('💩')); // -> false
+console.log(XRegExp('^\\pS$', 'A').test('💩')); // -> true
 XRegExp('(?A)^\\pS$').test('💩'); // -> true
 // Using surrogate pair U+D83D U+DCA9 to represent U+1F4A9 (pile of poo)
 XRegExp('(?A)^\\pS$').test('\uD83D\uDCA9'); // -> true
